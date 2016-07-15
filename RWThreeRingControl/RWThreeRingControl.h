5112264761b9f843be48029f0da2d7846e0b4b2b
carolight <caroline@archway.com.au>
2016-07-15 14:15:26 +1000

    Initial Commit

---
+++ RWThreeRingControl/RWThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  RWThreeRingControl.h
+//  RWThreeRingControl
+//
+//  Created by Caroline Begbie on 15/07/2016.
+//  Copyright © 2016 razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWThreeRingControl.
+FOUNDATION_EXPORT double RWThreeRingControlVersionNumber;
+
+//! Project version string for RWThreeRingControl.
+FOUNDATION_EXPORT const unsigned char RWThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWThreeRingControl/PublicHeader.h>
+
+
